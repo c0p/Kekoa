cc72245e930683d60fb957c3c3b9fa6b490a415f
c0p <jeraldydcascayan@gmail.com>
2018-05-20 20:03:07 -1000

    stats

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log('test')
+
+
+
+
+var teenCount = 0;
+var percCount = 0;
+
+function statCount() {
+    if (teenCount < 160000) {
+        teenCount += 250;
+        document.getElementById('num').innerHTML = teenCount;
+    }
+}
+
+
+function percentCount() {
+    if (percCount < 56) {
+        percCount++;
+        document.getElementById('num2').innerHTML = percCount;
+    }
+}
+
+setInterval(function () {
+    statCount();
+}, 1);
+
+setInterval(function () {
+    percentCount();
+}, 70);
